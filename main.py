a51f3003867d11f2d8ce150fda61881e85db3758
ccarluq0911 <ccarluq0911@g.educaand.es>
2025-01-19 11:20:31 +0100

    Primer commit

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import gymnasium as gym
+import ale_py
 
-gym.main()
+gym.register_envs(ale_py)
+env = gym.make("ALE/Breakout-v5")
+observation, info = env.reset()
+
+episode_over = False
+while not episode_over:
+    action = env.action_space.sample()  # agent policy that uses the observation and info
+    print(action)
+    observation, reward, terminated, truncated, info = env.step(action)
+    print(observation, reward, terminated, truncated, info)
+
+    episode_over = terminated or truncated
+
+env.close()
